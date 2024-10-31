6e2e1f8106bad94ea6b98ce95e0826113a27c891
olegmakeienko <molva83@gmail.com>
2024-10-31 09:27:48 +0100

    test for + and - when click

--- src/components/Counter.cy.jsx
+++ src/components/Counter.cy.jsx
@@ -22,4 +22,47 @@ describe('<Counter />', () => {
             randomButton.should("be.visible");
         })
     })
+
+    describe('0.2 användaren ska se talet 1 initialt (innan man klickat på någon knapp)', () => {
+        it("should display 1 initially", () => {
+            cy.get('.value').contains(1).should("be.visible");
+        } )
+    })
+
+    describe('1 "+" button ', () => {
+        let button;
+        beforeEach(() => {
+            button = cy.get(".add-button");
+        })
+
+        it('should display 2 after click on "+"', () => {
+            button.click();
+            cy.get(".value").contains(2).should("be.visible");
+        })
+
+        it('should increase value by 1 after every click on "+" ', () => {
+            button.click();
+            button.click();
+            button.click();
+            cy.get(".value").contains(4).should("be.visible");
+        })
+    })
+
+    describe('2 "-" button ', () => {
+
+        it('Should decrease value by 1 after every click on "-"', () => {
+            cy.get(".add-button").click();
+            cy.get(".subtract-button").click();
+            cy.get(".subtract-button").click();
+            cy.get(".value").contains(0).should("be.visible");
+        })
+
+        it('should decrease value only down to 0', () => {
+            cy.get(".subtract-button").click();
+            cy.get(".subtract-button").click();
+            cy.get(".subtract-button").click();
+            cy.get(".subtract-button").click();
+            cy.get(".value").contains(0).should("be.visible");
+        })
+    })
 })
